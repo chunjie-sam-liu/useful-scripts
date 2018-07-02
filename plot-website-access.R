42d820f8e64796fea80ac5bbb287d59434f46534
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-07-02 10:07:54 +0800

    add country stat

--- plot-website-access.R
+++ plot-website-access.R
@@ -4,6 +4,7 @@
 
 library(magrittr)
 library(ggplot2)
+library(rjson)
 
 
 # Lof files ---------------------------------------------------------------
@@ -41,7 +42,7 @@ range_time %>%
   labs(
     x = "Months",
     y = "Access count",
-    title = glue::glue("Total access IP is {length(log$date)}")
+    title = glue::glue("Total is {length(log$date)}, the IP is {length(unique(log$date))}")
   ) +
   theme(
     axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)
@@ -58,5 +59,61 @@ ggsave(
 
 
 # access by location ------------------------------------------------------
+freegeoip <- function(ip, format = ifelse(length(ip) == 1,'list','dataframe')) {
+  if (1 == length(ip))
+  {
+    # a single IP address
+    url <- paste(c("http://freegeoip.net/json/", ip), collapse = '')
+    ret <- fromJSON(readLines(url, warn = FALSE))
+    if (format == 'dataframe')
+      ret <- data.frame(t(unlist(ret)))
+    return(ret)
+  } else {
+    ret <- data.frame()
+    for (i in 1:length(ip))
+    {
+      r <- freegeoip(ip[i], format = "dataframe")
+      ret <- rbind(ret, r)
+    }
+    return(ret)
+  }
+} 
 
+log$ip %>% 
+  # head(10) %>% 
+  freegeoip() %>% 
+  tibble::as.tibble() %>% 
+  dplyr::select(-1) -> .t
 
+.t %>% 
+  dplyr::group_by(country_name) %>% 
+  dplyr::mutate(n = n()) %>% 
+  dplyr::distinct(country_name, n) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::arrange(dplyr::desc(n)) %>% 
+  dplyr::mutate(country_name = ifelse(country_name == "", "Unkown", country_name)) -> country_access
+
+lev <- country_access %>% dplyr::pull(country_name)
+
+country_access %>% 
+  dplyr::mutate(country_name = factor(country_name, levels = lev)) %>% 
+  ggplot(aes(x = country_name, y = n)) +
+  geom_bar(stat = "identity") + 
+  geom_text(aes(label = n), vjust = -0.5) +
+  ggthemes::theme_gdocs() +
+  theme(
+    axis.text.x = element_text(hjust = 1, vjust = 1, angle = 45)
+  ) +
+  labs(
+    x = "Country and region",
+    y = "Access count",
+    title = glue::glue("Total country and region is {length(country_access$country_name)}")
+  ) -> plot_country
+
+ggsave(
+  filename = paste(sub(pattern = ".log", "", filename),'country.pdf', sep = "-"), 
+  plot = plot_country, 
+  device = "pdf",
+  width = 5,
+  height = 5
+)
