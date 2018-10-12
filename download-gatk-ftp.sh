b681c7df4892ceaa5f005077d13c5bca2c525196
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-10-12 12:14:15 +0800

    download gatk reference hg38

--- download-gatk-ftp.sh
+++ download-gatk-ftp.sh
@@ -8,6 +8,31 @@
 #? username: gsapubftp-anonymous
 #? password:
 
-lftp -u gsapubftp-anonymous,'' \
-    -e "mirror -c bundle/hg38 $PWD; exit" \
-    ftp.broadinstitute.org
+# lftp -u gsapubftp-anonymous,'' \
+#     -e "mirror --continue --parallel=10 /bundle/hg38 $PWD; exit" \
+#     ftp.broadinstitute.org
+
+#? login
+host='ftp.broadinstitute.org'
+user='gsapubftp-anonymous'
+password=''
+
+#? remote directory
+remote_dir='/bundle/hg38'
+
+#? local direcotry
+local_dir='/home/liucj/data/data/refdata/bundle/hg38'
+
+#? runtime
+echo 
+echo "Starting download ${remote_dir} from ${host} to ${local_dir}"
+date
+
+lftp -u ${user},${password} ${host} <<EOF
+mirror --use-pget-n=60 ${remote_dir} ${local_dir};
+exit
+EOF
+
+echo 
+echo "Transfer finished"
+date
