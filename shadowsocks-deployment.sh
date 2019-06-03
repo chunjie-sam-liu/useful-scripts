ee75ded8d3500d8a5ad818bc7b6a7894921efffa
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2019-06-03 08:00:04 +0800

    reorder the code

--- shadowsocks-deployment.sh
+++ shadowsocks-deployment.sh
@@ -19,11 +19,6 @@ echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.conf
 # restart shadowsocks
 /etc/init.d/shadowsocks restart
 
-# run bbr
-wget --no-check-certificate https://raw.githubusercontent.com/chunjie-sam-liu/useful-scripts/master/shadowsocks-bbr.sh
-chmod +x shadowsocks-bbr.sh
-bash shadowsocks-bbr.sh
-
 # google scholar ipv6
 echo "2404:6800:4008:c06::be scholar.google.com" >> /etc/hosts
 echo "2404:6800:4008:c06::be scholar.google.com.hk" >> /etc/hosts
@@ -31,3 +26,10 @@ echo "2404:6800:4008:c06::be scholar.google.com.tw" >> /etc/hosts
 
 # restart shadowsocks
 /etc/init.d/shadowsocks restart
+
+# run bbr
+wget --no-check-certificate https://raw.githubusercontent.com/chunjie-sam-liu/useful-scripts/master/shadowsocks-bbr.sh
+chmod +x shadowsocks-bbr.sh
+bash shadowsocks-bbr.sh
+
+
