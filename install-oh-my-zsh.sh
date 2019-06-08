064627f5462a42923f751575038308810608f8bf
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2019-06-08 13:09:49 +0800

    intall-oh-my-zsh.sh

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -7,6 +7,7 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # add theme powerlevel9k
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+wget https://gist.githubusercontent.com/chunjie-sam-liu/976bd637294d5cc5dc92f1c6456624ad/raw/da3c9d89915bf6b54a2e392471cfb75776987dcd/.zsh
 
+git clone git@gist.github.com:976bd637294d5cc5dc92f1c6456624ad.git ~
 exec $SHELL
