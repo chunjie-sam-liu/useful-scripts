d1747d977b6ad917f533a3948d82e1126d4da6b4
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2019-06-09 09:09:04 +0800

    template slurm

---
+++ slurm-submit-jobs/demo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# demo.py:
+# usage: python demo.py [job-number]
+
+import sys
+import socket
+from time import sleep
+
+
+def work(jobnum):
+  print("Starting job {} on {}.".format(jobnum, socket.gethostname()))
+  sleep(5)
+  print("Finished job {}...\n".format(jobnum))
+
+
+if __name__ == "__main__":
+  jobnum = sys.argv[1]
+  work(jobnum)
