af87e4326800392b10577d8620efff49630cd369
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2019-09-26 18:26:16 +0800

    crontab to backup database to mount drive every two months.

--- crontab-backup-database-and-web.sh
+++ crontab-backup-database-and-web.sh
@@ -4,20 +4,20 @@
 # @DATE: 2019-09-26 10:32:59
 # @DESCRIPTION:
 
-# Number of input parameters
-
+mnt_dir=/mnt/database-backup
+backup_time=`date +'%Y-%m-%d'`
 
 # log files
-logs=/home/liucj/scripts/crontab-backup.logs
+logs=${mnt_dir}/backup-${backup_time}.logs
 exec &>> ${logs}
 
-echo "`date +%Y-%m-%d` Backup start."
+echo "${backup_time} Backup start."
 # root path
-root_dir=/home/backup-`date +%Y-%m-%d`
+root_dir=${mnt_dir}/backup-${backup_time}
 [[ -d ${root_dir} ]] && {
   echo "Backup data and scripts to ${root_dir}."
 } || {
-  echo "Path ${root_dir} not exists, create a new directory."
+  echo "Path ${root_dir} does not exists, create a new directory."
   mkdir ${root_dir}
   echo "Backup data and scripts to ${root_dir}."
 }
@@ -27,7 +27,7 @@ database_dir=${root_dir}/database
 [[ -d ${database_dir} ]] && {
   echo "Backup data to ${database_dir}."
 } || {
-  echo "Path ${database_dir} not exists, create a new directory."
+  echo "Path ${database_dir} does not exists, create a new directory."
   mkdir ${database_dir}
   echo "Backup data to ${database_dir}."
 }
@@ -35,18 +35,18 @@ database_dir=${root_dir}/database
 # MongoDB
 {
   echo "Start backup MongoDB."
-  mongo_dir=${database_dir}/mongo-all-data-`date +%Y-%m-%d`.mongo
+  mongo_dir=${database_dir}/mongo-all-data-${backup_time}.mongo
   mongodump --username 🙃 --password '🙃' --port 🙃 -o ${mongo_dir}
   tar -czf ${mongo_dir}.tar.gz ${mongo_dir}
   rm -rf ${mongo_dir}
-} &
+}
 
 # MySQL
 {
   echo "Start backup MySQL."
-  mysql_gz=mysql-all-data-`date +%Y-%m-%d`.sql.gz
+  mysql_gz=mysql-all-data-${backup_time}.sql.gz
   mysqldump -u🙃 -p🙃 -A --lock-tables=false | gzip > ${database_dir}/${mysql_gz}
-} &
+}
 
 
 # Scripts
@@ -55,7 +55,7 @@ web_dir=${root_dir}/web
 [[ -d ${web_dir} ]] && {
   echo "Backup web scripts to ${web_dir}."
 } || {
-  echo "Path ${web_dir} not exists, create a new directory."
+  echo "Path ${web_dir} does not exists, create a new directory."
   mkdir ${web_dir}
   echo "Backup web scripts to ${web_dir}."
 }
@@ -81,12 +81,32 @@ do
     tmp=${d#/home/}
     user=${tmp%%/*}
     webname=`basename ${d}`
-    gz=${user}-${webname}-`date +%Y-%m-%d`.tar.gz
+    gz=${user}-${webname}-${backup_time}.tar.gz
     cmd="tar -zcvf ${web_dir}/${gz} ${i}"
 
     {
       echo "Start backup ${user}'s ${webname} from ${i} to ${web_dir}/${gz}"
       eval ${cmd}
       echo "End zip ${webname}"
-    } &
+    }
 done
+
+
+# remove old backup
+
+backup_logs=(`ls -rt ${mnt_dir}/*.logs`)
+
+[[ ${#backup_logs[@]} -le 2 ]] && {
+  echo "Keep at least two backup."
+  exit 1
+} || {
+  echo "Remove old backups."
+  unset 'backup_logs[${#backup_logs[@]}-1]'
+  unset 'backup_logs[${#backup_logs[@]}-1]'
+  for i in ${backup_logs[@]};
+  do
+    cmd="rm -rf ${i%.logs}*"
+    echo ${cmd}
+    eval ${cmd}
+  done
+}
