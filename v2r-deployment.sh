53ee38d751d51a32150304d77af9f9320de5e3e9
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-04-08 18:52:11 +0800

    add v2r

---
+++ v2r-deployment.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# one key
+wget -N --no-check-certificate -q -O install.sh "https://raw.githubusercontent.com/chunjie-sam-liu/V2Ray-onekey/master/install.sh"
+chmod +x install.sh
+bash install.sh
+
+## 11 bbr accelerating scripts
+### 2 bbr plus core with <No> and restart vps
+
+#-----------------------------------
+# run install.sh again.
+bash install.sh
+
+## 11 bbr accelerating scripts
+### 7 bbr plus accelerating
+
+#-----------------------------------
+# run install.sh again.
+bash install.sh
+
+## 1 install v2ray (Nginx+ws+tls)
+### input domain (subdomain.domain.xxx)
+### connecting port: default 443
+### alterID: 55
