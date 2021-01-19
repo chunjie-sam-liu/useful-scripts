eb9e5f31a0786031414aa71da38bc1bb98d8bfb9
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-01-20 04:02:07 +0800

    pscode

---
+++ pscode.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2021-01-27 10:40:46
+# @DESCRIPTION:
+
+# Number of input parameters
+param=$#
+
+function fn_usage {}
+
+function main {}
